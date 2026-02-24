0fd3d32456156b2e0e4bb064373c30a0aa1c77b6
burdu <23202223@ibsu.edu.ge>
2026-02-24 18:16:53 +0400

    feature(86ewq920r): implement routing for auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AuthContainer } from './core/auth/container/auth-container';
+import { Login } from './core/auth/components/login/login';
+import { SignUp } from './core/auth/components/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthContainer,
+        children: [
+            { path: 'login', component: Login },
+            { path: 'register', component: SignUp },
+        ],
+    },
+];
---
+++ src/app/core/auth/components/sign-up/sign-up.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-sign-up',
+    imports: [],
+    templateUrl: './sign-up.html',
+    styleUrl: './sign-up.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignUp {}
