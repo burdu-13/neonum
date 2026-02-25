baaeb357e60ff1466222bc63804aecdd4913715b
burdu <23202223@ibsu.edu.ge>
2026-02-25 17:34:49 +0400

    feature(86ewqqpt0): create movie detail component structure

---
+++ src/app/features/movie-detail/container/movie-detailer-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-detailer-container',
+  imports: [],
+  templateUrl: './movie-detailer-container.html',
+  styleUrl: './movie-detailer-container.scss',
+})
+export class MovieDetailerContainer {
+
+}
