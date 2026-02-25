dc56fc5a9259b44c84a3e1db7476c64f47e2a86d
burdu <23202223@ibsu.edu.ge>
2026-02-25 16:41:48 +0400

    feature(86ewqcygx): improve design for header

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { UserStore } from '../../../store/user-info/user.store';
+import { BreakpointService } from '../../services/breakpoint-service/breakpoint-service';
 
 @Component({
     selector: 'app-header',
@@ -12,4 +13,26 @@ import { UserStore } from '../../../store/user-info/user.store';
 })
 export class Header {
     public readonly store = inject(UserStore);
+    public readonly bp = inject(BreakpointService);
+    public isMenuOpen = signal(false);
+
+    constructor() {
+        effect(() => {
+            if ((this.bp.isLargeTablet() || this.bp.isDesktop()) && this.isMenuOpen()) {
+                this.closeMenu();
+            }
+        });
+
+        effect(() => {
+            document.body.style.overflow = this.isMenuOpen() ? 'hidden' : 'auto';
+        });
+    }
+
+    public toggleMenu(): void {
+        this.isMenuOpen.set(!this.isMenuOpen());
+    }
+
+    public closeMenu(): void {
+        this.isMenuOpen.set(false);
+    }
 }
