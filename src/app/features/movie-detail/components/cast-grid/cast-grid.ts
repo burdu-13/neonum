c3d74dc4af6482e985e0167b310580cc6ee0dfdb
burdu <23202223@ibsu.edu.ge>
2026-02-26 19:40:09 +0400

    feature(86ewr3xr2): add router link to actor pages

--- src/app/features/movie-detail/components/cast-grid/cast-grid.ts
+++ src/app/features/movie-detail/components/cast-grid/cast-grid.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { CastMember } from '../../../../shared/models/movie.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-cast-grid',
-    imports: [MatIcon],
+    imports: [MatIcon,RouterLink],
     templateUrl: './cast-grid.html',
     styleUrl: './cast-grid.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
