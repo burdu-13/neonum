43ba3e89f8b5ba343f4a3893cf991529a401e041
burdu <23202223@ibsu.edu.ge>
2026-02-27 01:50:01 +0400

    fix(master): improve image optimization

--- src/app/features/dashboard/components/movie-card/movie-card.ts
+++ src/app/features/dashboard/components/movie-card/movie-card.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { Movie, TVShow } from '../../../../shared/models/movie.model';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
@@ -13,4 +13,22 @@ import { RouterLink } from '@angular/router';
 })
 export class MovieCard {
     public readonly movie = input.required<Movie | TVShow>();
+    public readonly priority = input<boolean>(false);
+
+    protected readonly displayData = computed(() => {
+        const item = this.movie();
+
+        const title = 'title' in item ? item.title : item.name;
+        const date = 'release_date' in item ? item.release_date : item.first_air_date;
+        const poster = item.poster_path;
+        const rating = item.vote_average;
+
+        return {
+            id: item.id,
+            title,
+            date,
+            poster,
+            rating,
+        };
+    });
 }
