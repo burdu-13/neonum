47ab20f6718357a14234cb21277f966c187ddb72
burdu <23202223@ibsu.edu.ge>
2026-02-28 01:23:28 +0400

    feature(86ewrgwth): implement dropdown component

--- src/app/features/explore/container/explore-container.ts
+++ src/app/features/explore/container/explore-container.ts
@@ -1,20 +1,32 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, OnInit, signal } from '@angular/core';
 import { ExploreStore } from '../store/explore.store';
 import { MovieCard } from '../../dashboard/components/movie-card/movie-card';
 import { Skeleton } from '../../../shared/components/skeleton/skeleton';
 import { CategoryPill } from '../../../shared/components/category-pill/category-pill';
 import { NnEmptyState } from '../../../shared/components/nn-empty-state/nn-empty-state';
 import { InteractionObserver } from '../../../shared/directives/interaction-observer';
+import { NnDropdown } from '../../../shared/components/nn-dropdown/nn-dropdown';
 
 @Component({
     selector: 'app-explore-container',
-    imports: [MovieCard, Skeleton, CategoryPill, NnEmptyState,InteractionObserver],
+    imports: [MovieCard, Skeleton, CategoryPill, NnEmptyState, InteractionObserver, NnDropdown],
     templateUrl: './explore-container.html',
     styleUrl: './explore-container.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeatureContainer implements OnInit {
     protected readonly exploreStore = inject(ExploreStore);
+
+    protected readonly sortValue = signal(this.exploreStore.filters().sort_by);
+
+    constructor() {
+        effect(() => {
+            const val = this.sortValue();
+            if (val !== this.exploreStore.filters().sort_by) {
+                this.exploreStore.updateFilters({ sort_by: val });
+            }
+        });
+    }
 
     public ngOnInit(): void {
         this.exploreStore.loadGenres();
