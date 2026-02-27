f88522bd66066f66e3c256835f0a6f81c327baa7
burdu <23202223@ibsu.edu.ge>
2026-02-27 16:48:31 +0400

    feature(86ewrf4dx): improve UI and UX for profile page

--- src/app/features/user-profile/container/user-profile-container.ts
+++ src/app/features/user-profile/container/user-profile-container.ts
@@ -1,17 +1,15 @@
 import { ChangeDetectionStrategy, Component, inject, input, OnInit } from '@angular/core';
 import { MovieShelf } from '../../dashboard/components/movie-shelf/movie-shelf';
-import { NnButton } from '../../../shared/components/nn-button/nn-button';
 import { UserStore } from '../../../store/user-info/user.store';
 import { MovieStore } from '../../../store/movie/movie.store';
-import { UserProfileStore } from '../../../store/user-profile/user-profile.store';
 import { ProfileStats } from '../components/profile-stats/profile-stats';
 import { ProfileHero } from '../components/profile-hero/profile-hero';
-import { Skeleton } from "../../../shared/components/skeleton/skeleton";
+import { Skeleton } from '../../../shared/components/skeleton/skeleton';
+import { NnEmptyState } from "../../../shared/components/nn-empty-state/nn-empty-state";
 
 @Component({
     selector: 'app-user-profile-container',
-    imports: [MovieShelf, ProfileStats, ProfileHero, Skeleton],
-    providers: [UserProfileStore],
+    imports: [MovieShelf, ProfileStats, ProfileHero, Skeleton, NnEmptyState],
     templateUrl: './user-profile-container.html',
     styleUrl: './user-profile-container.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,14 +17,8 @@ import { Skeleton } from "../../../shared/components/skeleton/skeleton";
 export class UserProfileContainer implements OnInit {
     protected readonly userStore = inject(UserStore);
     protected readonly movieStore = inject(MovieStore);
-    protected readonly profileStore = inject(UserProfileStore);
 
     public ngOnInit(): void {
-        const currentUserId = this.userStore.account()?.id;
-        if (currentUserId) {
-            this.profileStore.setProfileId(currentUserId);
-        }
-
         if (!this.movieStore.collectionsLoaded()) {
             this.movieStore.loadCollections();
         }
